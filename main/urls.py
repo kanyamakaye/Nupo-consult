cc52eb48407eb872aa626beefc94de005de2e69b
kanyamakaye <igicupuliofficial@gmail.com>
2025-06-28 18:59:22 +0200

    some change

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('designs/', views.design_more, name='designs'),
     path('analysis/', views.analysis_more, name='analysis'),
     path('engineering/', views.engineering_more, name='engineering'),
-    path('permitting/', views.permitting_more, name='permitting'),
+    path('permitting/contact/', views.permitting_more, name='permitting'),
     path('construction/', views.construction_more, name='construction'),
     path('engineering/', views.engineering_more, name='engineering_more'),
 ]
